aa01802b66d8f7414c17fe35c32ed58f4f5de0c3
Jhayr Cervantes <jhayrcervantes0981@gmail.com>
2026-05-29 15:41:59 +0800

    created supplier push notification

---
+++ apps/apsara-home-backend/app/Models/SupplierPushNotification.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SupplierPushNotification extends Model
+{
+    protected $table = 'tbl_supplier_push_notifications';
+    protected $primaryKey = 'spn_id';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'spn_supplier_id',
+        'spn_title',
+        'spn_body',
+        'spn_image',
+        'spn_recipients',
+        'spn_sent_count',
+        'spn_failed_count',
+        'spn_sent_at',
+        'spn_created_at',
+        'spn_updated_at',
+    ];
+
+    protected $casts = [
+        'spn_recipients' => 'array',
+        'spn_sent_at' => 'datetime',
+        'spn_created_at' => 'datetime',
+        'spn_updated_at' => 'datetime',
+    ];
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class, 'spn_supplier_id');
+    }
+}
