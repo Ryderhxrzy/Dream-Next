06ccfa5f53f72c12ca2fa643d651904af1087bd0
TryAgain1122 <luisrafhael1122@gmail.com>
2026-06-12 10:35:20 +0800

    fixed github actions and removed playwright e2e testiings

--- apps/e2e/home.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test("loads the public home page", async ({ page }) => {
-  const response = await page.goto("/");
-
-  expect(response?.ok()).toBeTruthy();
-  await expect(page).toHaveTitle(/AF Home|Home/i);
-  await expect(page.locator("body")).toBeVisible();
-});
